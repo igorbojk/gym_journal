a2b7d7642029e6434fabc5ea78ecb09a697df63f
Igor <igorbojk@gmail.com>
2018-04-24 15:30:21 +0300

    fix statistic bug

--- src/pages/statistic/statistic.ts
+++ src/pages/statistic/statistic.ts
@@ -52,10 +52,11 @@ export class StatisticPage implements OnInit{
   generateExercisesMaxWeight() {
     this.trainings.forEach((element) => {
       this.trainingsDates[element.title] = this.statisticService.getTrainingDates(element.title);
+      this.chartData[element.title]={};
       element.exercises.forEach((i) => {
         const data = this.statisticService.getExerciseMaxWeight(element.title, i.title);
-        this.chartData[i.title] = [];
-        this.chartData[i.title].push({data});
+        this.chartData[element.title][i.title] = [];
+        this.chartData[element.title][i.title].push({data});
       });
     });
   }
